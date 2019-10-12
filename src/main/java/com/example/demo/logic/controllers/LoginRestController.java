009dfd29b08a814758b5d2282bbf4ba404f90c89
Jimmy <wanadooht@gmail.com>
2019-10-12 19:18:33 +0500

    Logger

--- src/main/java/com/example/demo/logic/controllers/LoginRestController.java
+++ src/main/java/com/example/demo/logic/controllers/LoginRestController.java
@@ -3,6 +3,8 @@ package com.example.demo.logic.controllers;
 import com.example.demo.logic.InfoContainer;
 import com.example.demo.logic.User;
 import com.example.demo.logic.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,22 +16,26 @@ import javax.validation.Valid;
 @RequestMapping("/login")
 public class LoginRestController {
     public UserRepository user;
-
+    Logger logger = LoggerFactory.getLogger(LoginRestController.class);
 
     @GetMapping
     public User get(){
+        logger.info("Show user state.");
         return InfoContainer.getUser();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity logInForm(@Valid @RequestBody String name) {
         // TODO use right way of initialize
+        logger.info("User " + name + "try to log in.");
         if (user.existsById(name)) {
             InfoContainer.StartNewSession(user.getOne(name));
             return ResponseEntity.ok().build();
         }
         else
-            return ResponseEntity.notFound().build();
+            {
+                return ResponseEntity.notFound().build();
+            }
     }
 
     @DeleteMapping
